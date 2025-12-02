96582284866ee2c3f66f798f42e50c3c79fc1c0e
unknown <a01256421@tec.mx>
2025-12-02 14:41:17 -0800

    Agrega la clase producto

---
+++ Producto.h
@@ -0,0 +1,36 @@
+//
+// Created by Hp on 01/12/2025.
+//
+
+#ifndef UNTITLED28_PRODUCTO_H
+#define UNTITLED28_PRODUCTO_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Producto {
+private:
+    string nombre;
+    int cantidad;
+    double precio;
+
+public:
+    Producto();
+    Producto(string cNombre, int cCantidad, double cPrecio);
+
+    string getNombre();
+    int getCantidad();
+    double getPrecio();
+
+    void setNombre(string n);
+    void setCantidad(int c);
+    void setPrecio(double p);
+
+    friend ostream & operator<<(ostream &os, const Producto &d);
+
+    ~Producto();
+
+};
+
+
+#endif //UNTITLED28_PRODUCTO_H
